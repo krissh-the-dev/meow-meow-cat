a0884a0a72f5560c35747856a88c1fc17576756f
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-06-27 16:52:55 +0530

    initial commit - mmc made open source version 2.4

---
+++ app/src/main/java/akris/in/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package akris.in.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import akris.in.myapplication.R;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonTap(View v){
+        Toast myToast = Toast.makeText(getApplicationContext(), "Meow! <3", Toast.LENGTH_LONG);
+        myToast.show();
+        MediaPlayer meow = MediaPlayer.create(MainActivity.this, R.raw.meow);
+        meow.start();
+    }
+}
