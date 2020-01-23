076dd9e7284ad499445cb4098ff419fdadaef7c9
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2020-01-23 23:01:53 +0530

    added more sounds and rewritten

--- app/src/main/java/akris/in/myapplication/MainActivity.java
+++ app/src/main/java/akris/in/myapplication/MainActivity.java
@@ -1,25 +1,36 @@
 package akris.in.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import akris.in.myapplication.R;
-
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+    int[] audio = new int[4];
+    int iteration = 0;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void ButtonTap(View v){
-        Toast myToast = Toast.makeText(getApplicationContext(), "Meow! 🐱", Toast.LENGTH_LONG);
-        myToast.show();
-        MediaPlayer meow = MediaPlayer.create(MainActivity.this, R.raw.meow);
-        meow.start();
+        audio[0] = R.raw.meow1;
+        audio[1] = R.raw.meow2;
+        audio[2] = R.raw.meow4;
+        audio[3] = R.raw.meow4;
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast myToast = Toast.makeText(getApplicationContext(), "Meow! 🐱", Toast.LENGTH_LONG);
+                myToast.show();
+                MediaPlayer meow = MediaPlayer.create(MainActivity.this, audio[iteration]);
+                meow.start();
+                iteration += 1;
+                if (iteration > 3)
+                    iteration = 0;
+            }
+        });
     }
 }
